d0631ccbe2c8c99db18f9a5c3fd7a3d73aa5eb34
Property404 <daganboy@gmail.com>
2016-11-21 21:19:58 +0000

    Added comments, added automatic referring of / to index

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
-const express = require("express");
-var path = require("path"),
+const express = require("express"),
+path = require("path"),
 url = require("url"),
 exec = require("child_process").exec,
 fs = require("fs"),
 bodyParser = require("body-parser"),
 shellescape = require("shell-escape");
+
+
+/* Execute a shell command and send back stdout and stdout */
 function execute(command, callback){
 	exec(command, function(error, stdout, stderr){callback(stdout, stderr);});
 };
-//bah abah bah
+
 /* Set up app */
 var app = express();
+
 //* Set up bloo blap
 app.use(bodyParser.urlencoded({
     extended: true
@@ -20,15 +24,16 @@ app.use(bodyParser.urlencoded({
  *  * Parses the text as JSON and exposes the resulting object on req.body.
  *   */
 app.use(bodyParser.json());
-// Static Pages
-app.use("/static", express.static("./public"));
 
-// Dynamic Pageos 
-app.get('/:bf',(req, res)=> bfpage(req, res));
-app.post('/:bf',(req, res)=> bfpage(req, res));
+// Handle dynamic Pageos through get/post
+app.get('/:bf',(req, res)=> bfpage(req, res, false));
+app.post('/:bf',(req, res)=> bfpage(req, res, false));
+app.get('/', (req, res) => bfpage(req, res, true));
 
-function bfpage(req, res) {
-	var path = "./public/"+req.path;
+// Process request for a Brainfuck page
+function bfpage(req, res, isindex) {
+	var path = "./public/"+(isindex?"index.bf":req.path);
+	console.log(path);
 	fs.stat(path, function(err, stats){
 		if(!err && !stats.isFile()){
 			res.status(400).send("Ayy 404 lmao");
@@ -46,7 +51,6 @@ function bfpage(req, res) {
 				postreq+="\\0";
 				shult = shellescape(["echo", "-e", postreq])+"|"+shellescape(["bf", "-od", path])
 				execute(shult, function(output, errput){
-							console.log(err);
 							if(path.indexOf(".css")!==-1){
 								res.set("Content-type","text/css");
 							}
